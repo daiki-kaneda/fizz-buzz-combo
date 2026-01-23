3d9cbb2acd03e9310c35819e1e7bf2de1915d759
daiki-kaneda <daiki0622developing@gmail.com>
2026-01-23 14:32:10 +0900

    chore: initial commit

---
+++ src/main/java/com/example/fizz_buzz_combo/FizzBuzzComboApplication.java
@@ -0,0 +1,13 @@
+package com.example.fizz_buzz_combo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzComboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzBuzzComboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fizz_buzz_combo/FizzBuzzComboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fizz_buzz_combo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzBuzzComboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
