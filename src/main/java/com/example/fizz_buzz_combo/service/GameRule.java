6403a05fdbd54e7aad34008ace364fb4a4cb192e
daiki-kaneda <daiki0622developing@gmail.com>
2026-01-24 16:18:20 +0900

    feat(game): add game rule interface,game service

---
+++ src/main/java/com/example/fizz_buzz_combo/service/GameRule.java
@@ -0,0 +1,28 @@
+package com.example.fizz_buzz_combo.service;
+
+import java.util.List;
+
+public interface GameRule {
+    String getId();
+    String getName();
+    String getRarity(); // common,uncommon,rare
+    List<String> getTags(); // tag: multiple,combo,special
+
+    // 発動条件
+    boolean shouldApply(int number);
+
+    // 基本スコア
+    default int getScore(){
+        return 0;
+    }
+
+    // 倍数(倍数ルールの時のみ)
+    default Integer getMultiple(){
+        return null;
+    }
+
+    // 倍率（comboルールの時のみ)
+    default double getMultiplier(List<GameRule> rules){
+        return 1.0;
+    }
+}
---
+++ src/main/java/com/example/fizz_buzz_combo/service/GameService.java
@@ -0,0 +1,56 @@
+package com.example.fizz_buzz_combo.service;
+
+import java.util.List;
+import java.util.random.RandomGenerator;
+import java.util.stream.Collectors;
+
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+
+import com.example.fizz_buzz_combo.entity.PlayLog;
+import com.example.fizz_buzz_combo.entity.User;
+import com.example.fizz_buzz_combo.repository.GameRuleQueryRepository;
+import com.example.fizz_buzz_combo.repository.PlayLogRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GameService {
+    private final UserService userService;
+    private final GameRuleQueryRepository gameRuleQueryRepository;
+    private final PlayLogRepository playLogRepository;
+    private final List<GameRule> gameRules;
+
+    public PlayLog start(Jwt jwt, List<String> ruleIds) {
+        RandomGenerator random = RandomGenerator.getDefault();
+        int generatedNumber = random.nextInt(1, 101);
+        int score = (int) calculateScore(generatedNumber, ruleIds);
+
+        List<com.example.fizz_buzz_combo.entity.GameRule> rules = gameRuleQueryRepository.findAllByIdIn(ruleIds);
+        User user = userService.getOrCreateUser(jwt);
+        PlayLog log = PlayLog.create(generatedNumber, score, user, rules);
+
+        return playLogRepository.save(log);
+    }
+
+    private long calculateScore(int generatedNumber, List<String> ruleIds) {
+        // 適用ルールを取得
+        List<GameRule> appliedRules = gameRules
+                .stream()
+                .filter(rule -> ruleIds.contains(rule.getId()))
+                .filter(rule -> rule.shouldApply(generatedNumber))
+                .toList();
+        // ベーススコアの計算
+        int baseScore = appliedRules
+                .stream()
+                .collect(Collectors.summingInt(rule -> rule.getScore()));
+        // 倍率を計算(コンボなど)
+        double multiplier = appliedRules
+                .stream()
+                .map(rule -> rule.getMultiplier(appliedRules))
+                .reduce(1.0, (a, b) -> a * b);
+        // 浮動小数点数を整数に四捨五入
+        return Math.round(baseScore * multiplier);
+    }
+}
