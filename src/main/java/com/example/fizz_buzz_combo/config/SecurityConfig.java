a0ac0614613b91efa113c4fcbd3542f205f35648
daiki-kaneda <daiki0622developing@gmail.com>
2026-01-24 11:40:51 +0900

    feat(auth): add custom jwt decoder for check audience

--- src/main/java/com/example/fizz_buzz_combo/config/SecurityConfig.java
+++ src/main/java/com/example/fizz_buzz_combo/config/SecurityConfig.java
@@ -5,18 +5,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtAudienceValidator;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.fizz_buzz_combo.config.properties.JwtProperties;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtProperties jwtProperties;
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http){
+    SecurityFilterChain filterChain(HttpSecurity http) {
         return http
-        .csrf(csrf->csrf.disable())
-        .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()))
-        .build();
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
     }
 
-    
+    @Bean
+    JwtDecoder jwtDecoder() {
+        String issuerUri = jwtProperties.getIssuerUri();
+        String expectedAudience = jwtProperties.getExpectedAudience();
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromIssuerLocation(issuerUri);
+
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+        OAuth2TokenValidator<Jwt> audienceValidator = new JwtAudienceValidator(expectedAudience);
+
+        jwtDecoder.setJwtValidator(new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator));
+        return jwtDecoder;
+    }
 }
