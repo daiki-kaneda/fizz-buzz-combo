a38bd2b2aa282958f4d994f4571192642d807ec6
daiki-kaneda <daiki0622developing@gmail.com>
2026-01-26 13:48:14 +0900

    fix(play-log): fix N+1 problem in ranking retrieval

--- src/main/java/com/example/fizz_buzz_combo/repository/PlayLogQueryRepository.java
+++ src/main/java/com/example/fizz_buzz_combo/repository/PlayLogQueryRepository.java
@@ -2,12 +2,15 @@ package com.example.fizz_buzz_combo.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.Repository;
 
 import com.example.fizz_buzz_combo.entity.PlayLog;
 
 public interface PlayLogQueryRepository extends Repository<PlayLog,Long>{
+    @EntityGraph(attributePaths = {"user", "logRules.gameRule"})
     List<PlayLog> findTop5ByOrderByScoreDesc();
 
+    @EntityGraph(attributePaths = {"user", "logRules.gameRule"})
     List<PlayLog> findTop5ByUserUidOrderByScoreDesc(String uid);
 }
