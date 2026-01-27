4860313e173cc6b97ab27d9223f8b367976b9fb2
daiki-kaneda <daiki0622developing@gmail.com>
2026-01-27 11:14:04 +0900

    chore(audit): enable jpa auditing

--- src/main/java/com/example/fizz_buzz_combo/FizzBuzzComboApplication.java
+++ src/main/java/com/example/fizz_buzz_combo/FizzBuzzComboApplication.java
@@ -2,8 +2,10 @@ package com.example.fizz_buzz_combo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FizzBuzzComboApplication {
 
 	public static void main(String[] args) {
